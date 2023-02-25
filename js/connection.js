4b19e451547376acf646c803caf2fc0e6bf2cd58
Douglas Navarrete <dranzersv@gmail.com>
2023-02-25 09:04:17 -0600

    feature: added method to update, but it is incomplete

--- js/connection.js
+++ js/connection.js
@@ -16,5 +16,29 @@ class connection {
     const data = await response.json();
     return data;
   }
+  async createPost() {
+    const url = 'https://harmless-polydactyl-hiss.glitch.me/posts/';
+    const post = {
+      title: 'curlesca',
+      subTitle: 'okarina',
+      image: 'https://i.ytimg.com/vi/INk1W8OujQI/maxresdefault.jpg',
+      body: 'Banshee is an all-around fighter whodfsdfsdfsdqwuu uuwto respond to any situation. In Super Smash Bros. Ultimate, he shows up in his Wedding tux and his Builder outfit, and Cappy even makes an appearance!',
+      createDate: '2018/06/18',
+      likes: 12,
+      author: 3,
+      tags: [1, 6],
+    };
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(post),
+    };
+
+    const response = await fetch(url, requestOptions);
+    const data = await response.json();
+    console.log('Respuesta:', data);
+  }
 }
 let query = new connection('https://harmless-polydactyl-hiss.glitch.me/');
--- js/utils.js
+++ js/utils.js
@@ -4,3 +4,13 @@ function renderElement(element, container, className) {
   div.innerHTML = element;
   container.appendChild(div);
 }
+
+function dateNow() {
+  // Crea un objeto Date para la fecha actual
+  const date = new Date();
+
+  // Crea una cadena de texto con la fecha en el formato deseado
+  const dateString = date.toISOString().substring(0, 10).replace(/-/g, '/');
+
+  return dateString;
+}
